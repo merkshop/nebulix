8e420853bd013c34cabe06e44edfcac2eadbd7df
merkshop <merkshop2024@gmail.com>
2024-06-30 17:13:07 +0000

    se cambia nombre de sitio en la configuración de astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,7 @@ const m2dxOptions = {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nebulix.unfolding.io",
+  site: "https://merkshop.com.mx",
   integrations: [
     icon(),
     mdx({}),
